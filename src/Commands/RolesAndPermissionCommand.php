1cfaf12f6a775d5ae2ac6aa36defb84bdad6c06d
mt2812 <maxtaylor281201@gmail.com>
2023-12-20 16:09:54 +0000

    Start first command

---
+++ src/Commands/RolesAndPermissionCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RolesAndPermissionCommand extends Command
+{
+    protected $signature = 'novatura:roles:install';
+
+    protected $description = 'Create all the files required a roles and permissions based architecture using gates';
+
+    public function handle(){
+        
+    }
+
+
+}
