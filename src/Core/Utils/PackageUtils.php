96629b5bf617685131cdc437abe420ce38a2e17f
Thomas Mitchelmore <tommitchelmore@outlook.com>
2023-12-21 00:27:49 +0000

    feat: dependency installing works

---
+++ src/Core/Utils/PackageUtils.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Novatura\Laravel\Core\Utils;
+
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+
+/**
+ * Package utils trait
+ */
+trait PackageUtils
+{
+    /**
+     * Require composer packages
+     * 
+     * @param array $packages composer packages to require
+     * @param bool $asDev require as dev dependencies?
+     */
+    function requireComposerPackages(array $packages, $asDev = false)
+    {
+        $command = array_merge(
+            ['composer', 'require'],
+            $asDev ? ['--dev'] : [],
+            $packages
+        );
+
+        return (new Process($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => -1]))
+            ->setTimeout(null)
+            ->run(function ($type, $output) {
+                $this->output->write($output);
+            }) === 0;
+    }
+
+    /**
+     * Update node packages
+     * 
+     * @param callable $callback callback to update packages - (existingPcks[]) -> newPcks[]
+     * @param bool $dev update dev dependencies?
+     */
+    function updateNodePackages(callable $callback, $dev = true)
+    {
+        if (! file_exists(base_path('package.json'))) {
+            return;
+        }
+
+        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $packages[$configurationKey] = $callback(
+            array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [],
+            $configurationKey
+        );
+
+        ksort($packages[$configurationKey]);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
+        );
+    }
+
+    /**
+     * Flush node modules
+     */
+    function flushNodeModules()
+    {
+        tap(new Filesystem, function ($files) {
+            $files->deleteDirectory(base_path('node_modules'));
+            $files->delete(base_path('yarn.lock'));
+            $files->delete(base_path('package-lock.json'));
+            $files->delete(base_path('bun.lockb'));
+        });
+    }
+
+    /**
+     * Install node modules
+     * Automatically detects:
+     * - package-lock.json -> npm
+     * - yarn.lock -> yarn
+     * - bun.lockb -> bun
+     * - else, npm
+     */
+    function installNodeModules()
+    {
+        $manager = 'npm';
+        $cmd = 'install';
+
+        if (file_exists(base_path('package-lock.json'))) {
+            $manager = 'npm';
+            $cmd = 'ci';
+        } elseif (file_exists(base_path('yarn.lock'))) {
+            $manager = 'yarn';
+            $cmd = 'install';
+        } elseif (file_exists(base_path('bun.lockb'))) {
+            $manager = 'bun';
+            $cmd = 'install';
+        }
+
+        return (new Process([$manager, $cmd], base_path(), ['COMPOSER_MEMORY_LIMIT' => -1]))
+            ->setTimeout(null)
+            ->run(function ($type, $output) {
+                $this->output->write($output);
+            }) === 0;
+    }
+}
---
+++ src/Scaffold/Commands/InstallCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Novatura\Laravel\Scaffold\Commands;
+
+use Illuminate\Console\Command;
+use Novatura\Laravel\Core\Utils\PackageUtils;
+
+class InstallCommand extends Command
+{
+    use PackageUtils;
+
+    protected $signature = 'novatura:scaffold:install {--auth}';
+    protected $description = 'Install Novatura Frontend/Auth Scaffold';
+    
+    /**
+     * Execute the console command.
+     * 
+     * @return int|null
+     */
+    public function handle()
+    {
+        $this->installFrontend();
+        if ($this->option('auth')) {
+            $this->installAuth();
+        }
+    }
+
+    protected function installFrontend()
+    {
+        echo "Installing frontend scaffold...\n";
+
+        /**
+         * Composer packages
+         */
+        if (!$this->requireComposerPackages([
+            'inertiajs/inertia-laravel:^0.6.3',
+            'tightenco/ziggy:^1.0',
+        ])) {
+            echo "Composer packages installation failed.\n";
+            throw new \Exception("Composer packages installation failed.");
+        }
+
+        /**
+         * NPM Packages
+         */
+        $this->updateNodePackages(function ($packages) {
+            return [
+                '@inertiajs/react' => '^1.0.14',
+                'react' => '^18.2.0',
+                'react-dom' => '^18.2.0',
+                '@mantine/core' => '^7.3.2',
+                '@mantine/hooks' => '^7.3.2',
+                '@mantine/nprogress' => '^7.3.2',
+            ] + $packages;
+        }, false);
+        $this->updateNodePackages(function ($packages) {
+            return [
+                '@vitejs/plugin-react' => '^4.2.1',
+                'postcss' => '^8.4.32',
+                'postcss-preset-mantine' => '^1.12.0',
+                'postcss-simple-vars' => '^7.0.1',
+                '@types/node' => '^20.10.5',
+                '@types/react' => '^18.2.45',
+                '@types/react-dom' => '^18.2.18',
+                'typescript' => '^5.3.3'
+            ] + $packages;
+        }, true);
+        $this->installNodeModules();
+
+
+    }
+
+    protected function installAuth()
+    {
+        echo "Installing auth scaffold...\n";
+
+        /**
+         * Composer packages
+         */
+        if (!$this->requireComposerPackages([
+            'laravel/sanctum:^3.2',
+        ])) {
+            throw new \Exception("Composer packages installation failed.");
+        }
+    }
+}
