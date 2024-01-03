c5a9fcf1d73f67ab36f9377bcaff90293297cf2d
Thomas Mitchelmore <tommitchelmore@outlook.com>
2024-01-03 16:57:51 +0000

    feat: implement mantine nprogress

--- src/Scaffold/stubs/resources/js/Providers.tsx
+++ src/Scaffold/stubs/resources/js/Providers.tsx
@@ -2,13 +2,36 @@ import { ReactNode } from "react";
 import Theme from "@/Theme";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { NavigationProgress, nprogress } from '@mantine/nprogress';
+import { router } from "@inertiajs/react";
 
 import '@mantine/core/styles.css'
 import '@mantine/notifications/styles.css';
+import '@mantine/nprogress/styles.css';
 
 function Providers({ children }: ProvidersProps) {
+
+    router.on('start', () => nprogress.start())
+    router.on('progress', (ev) => {
+        if (ev.detail.progress?.percentage) {
+            nprogress.set(ev.detail.progress.percentage)
+        }
+    })
+    router.on('finish', (ev) => {
+        if (ev.detail.visit.completed) {
+            nprogress.complete()
+        } else if (ev.detail.visit.interrupted) {
+            nprogress.reset()
+        } else if (ev.detail.visit.cancelled) {
+            nprogress.reset()
+        } else {
+            nprogress.reset()
+        }
+    })
+
     return (
         <MantineProvider theme={Theme}>
+            <NavigationProgress />
             <Notifications position="top-center" />
             {children}
         </MantineProvider>
