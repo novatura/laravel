962b2f36b53d05734130b9cfa213e916ef3be66e
mt2812 <maxtaylor281201@gmail.com>
2024-01-08 09:12:41 +0000

    Update stub dir

--- src/RolesAndPermissions/Commands/Install.php
+++ src/RolesAndPermissions/Commands/Install.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 
 class Install extends Command
 {
-    protected $signature = 'novatura:roles:install {--c|controllers : Include controllers} {--s|seeder : Include a Role Seeder}';
+    protected $signature = 'novatura:roles:install';
 
     protected $description = 'Create files for a roles and permissions based architecture using gates';
 
@@ -23,35 +23,42 @@ class Install extends Command
 
     public function handle()
     {
-        $generateFiles = [
-            ['path' => app_path('Models/Permission.php'), 'stub' => 'permission_model.stub'],
-            ['path' => app_path('Models/Role.php'), 'stub' => 'role_model.stub'],
 
-            ['path' => database_path("migrations/{$this->getCurrentTimestamp()->subSecond()->format('Y_m_d_His')}_create_permissions_table.php"), 'stub' => 'permission_migration.stub'],
-            ['path' => database_path("migrations/{$this->getCurrentTimestamp()->subSecond()->format('Y_m_d_His')}_create_roles_table.php"), 'stub' => 'role_migration.stub'],
-            ['path' => database_path("migrations/{$this->getCurrentTimestamp()->format('Y_m_d_His')}_create_role_permissions_table.php"), 'stub' => 'role_permission_migration.stub'],
+                /**
+         * Copy file structure from ../stubs to project
+         */
+        $this->info("Copying new files...");
+        File::copyDirectory(__DIR__ . '/../stubs', base_path());
 
-            ['path' => app_path('providers/PermissionGateProvider.php'), 'stub' => 'permission_provider.stub'],
-        ];
+        // $generateFiles = [
+        //     ['path' => app_path('Models/Permission.php'), 'stub' => 'permission_model.stub'],
+        //     ['path' => app_path('Models/Role.php'), 'stub' => 'role_model.stub'],
 
-        if ($this->option('controllers')) {
-            // Only add controllers if the -c option is true
-            $generateFiles = array_merge($generateFiles, [
-                ['path' => app_path('Http/Controllers/PermissionController.php'), 'stub' => 'permission_controller.stub'],
-                ['path' => app_path('Http/Controllers/RoleController.php'), 'stub' => 'role_controller.stub'],
-            ]);
-        }
+        //     ['path' => database_path("migrations/{$this->getCurrentTimestamp()->subSecond()->format('Y_m_d_His')}_create_permissions_table.php"), 'stub' => 'permission_migration.stub'],
+        //     ['path' => database_path("migrations/{$this->getCurrentTimestamp()->subSecond()->format('Y_m_d_His')}_create_roles_table.php"), 'stub' => 'role_migration.stub'],
+        //     ['path' => database_path("migrations/{$this->getCurrentTimestamp()->format('Y_m_d_His')}_create_role_permissions_table.php"), 'stub' => 'role_permission_migration.stub'],
 
-        if ($this->option('seeder')) {
-            // Only add controllers if the -c option is true
-            $generateFiles = array_merge($generateFiles, [
-                ['path' => database_path('seeders/RoleSeeder.php'), 'stub' => 'role_seeder.stub'],
-            ]);
-        }
+        //     ['path' => app_path('providers/PermissionGateProvider.php'), 'stub' => 'permission_provider.stub'],
+        // ];
+
+        // if ($this->option('controllers')) {
+        //     // Only add controllers if the -c option is true
+        //     $generateFiles = array_merge($generateFiles, [
+        //         ['path' => app_path('Http/Controllers/PermissionController.php'), 'stub' => 'permission_controller.stub'],
+        //         ['path' => app_path('Http/Controllers/RoleController.php'), 'stub' => 'role_controller.stub'],
+        //     ]);
+        // }
+
+        // if ($this->option('seeder')) {
+        //     // Only add controllers if the -c option is true
+        //     $generateFiles = array_merge($generateFiles, [
+        //         ['path' => database_path('seeders/RoleSeeder.php'), 'stub' => 'role_seeder.stub'],
+        //     ]);
+        // }
 
 
 
-        (new MakeFile($this, $generateFiles))->generate();
+        // (new MakeFile($this, $generateFiles))->generate();
         $this->comment("\nTo complete the setup:\n - Migrate the new database files\n - Add the roles relationship to the user model\n - Include the PermissionGateProvider in the app config file");
     }
 
