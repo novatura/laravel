c1e0c2cd8d3bedbef86db67f58f4efe16481b56a
mt2812 <maxtaylor281201@gmail.com>
2024-01-11 18:22:32 +0000

    Finish history install command

--- src/UserHistory/Commands/Install.php
+++ src/UserHistory/Commands/Install.php
@@ -5,9 +5,13 @@ namespace Novatura\Laravel\UserHistory\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Carbon\Carbon;
+use Novatura\Laravel\Core\Utils\FileUtils;
 
 class Install extends Command
 {
+
+    use FileUtils; 
+
     protected $signature = 'novatura:history:install {--c|controllers : Include controllers}';
 
     protected $description = 'Create files for tracking user history with dedicated Model Macros';
@@ -26,6 +30,15 @@ class Install extends Command
          */
         $this->info("Copying new files...");
         File::copyDirectory(__DIR__ . '/../stubs', base_path());
+
+        $this->info('Binding Repositories...');
+        $this->call('novatura:bind:repository', [
+            'modelName' => 'Novatura\\Laravel\\UserHistory\\Lib\\Models\\History' 
+        ]);
+
+        $this->addRoutes(['auth'], [
+            "Route::get('/history', [\App\Http\Controllers\HistoryController::class, 'index'])->middleware('can:history.index')->name('history.index');",
+        ]);
 
         $this->comment("\nTo complete the setup:\n - Migrate the new database files\n - Add the roles relationship to the user model\n - Include the PermissionGateProvider in the app config file");
     }
--- src/UserHistory/stubs/app/Http/Controllers/HistoryController.php
+++ src/UserHistory/stubs/app/Http/Controllers/HistoryController.php
@@ -18,13 +18,6 @@ class HistoryController extends Controller
 
     public function index(){
 
-        \App\Models\User::createWithHistory([
-            'first_name' => 'John',
-            'last_name' => 'Baker',
-            'email' => 'john@example.com',
-            'password' => 'password',
-        ], 'Creating a new User');
-
         return Inertia::render('History/History', [
             'history' => $this->historyRepository->getAllHistory(),
         ]);
