7c2680c03ef77fb6074bedcf5dd6610b65596141
mt2812 <maxtaylor281201@gmail.com>
2024-01-12 14:39:50 +0000

    Add colour to migration for creating roles

--- src/RolesAndPermissions/stubs/database/migrations/2024_01_05_140124_create_roles_table.php
+++ src/RolesAndPermissions/stubs/database/migrations/2024_01_05_140124_create_roles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->string('colour')->default('#238BE6');
             $table->timestamps();
         });
     }
