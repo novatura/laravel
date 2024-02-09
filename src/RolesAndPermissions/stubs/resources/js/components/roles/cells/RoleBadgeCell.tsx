6969453923b6f7cb8b1341f00cb893825ac7cb77
Thomas Mitchelmore <tommitchelmore@outlook.com>
2024-02-09 09:47:32 +0000

    fix: broke it

--- src/RolesAndPermissions/stubs/resources/js/components/roles/cells/RoleBadgeCell.tsx
+++ src/RolesAndPermissions/stubs/resources/js/components/roles/cells/RoleBadgeCell.tsx
@@ -2,7 +2,7 @@ import RoleBadge from "@/components/roles/RoleBadge"
 import CustomCell from "@/lib/CustomCell"
 import { Role } from "@/types/Role"
 
-const RoleBadgeCell = CustomCell<Role>(({ row: { original } }) => {
+const RoleBadgeCell = CustomCell<Role & { isGranted: boolean }>(({ row: { original } }) => {
     return (
         <RoleBadge size="lg" {...original} />
     )
